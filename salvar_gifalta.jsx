e720a5fd761911c32d2dae7826ad11c3d094c37e
Maiane Gabriele de Araujo <59965402+maianegabriele@users.noreply.github.com>
2020-01-16 11:38:15 -0300

    Add files via upload

---
+++ salvar_gifalta.jsx
@@ -0,0 +1,33 @@
+﻿doc = activeDocument
+
+function salvarGIF (nome){
+        
+        var meuArquivo = new File(doc.path + "/" + nome + ".gif")
+        
+        var options = new ExportOptionsSaveForWeb();
+        options.format = SaveDocumentType.COMPUSERVEGIF;
+        options.colorReduction = ColorReductionType.SELECTIVE;
+        options.dither = Dither.NONE;
+        options.quality = 0;
+        options.colors = 256;
+        options.blur = 0.0;
+        options.transparencyDither = Dither.NONE
+        options.ditherAmout = 100
+        options.includeProfile = false
+        options.interlaced = true
+        options.lossy = 0
+        options.optimized = true
+        //options.quality = 100 :( zoou tudo.
+        options.transparency = true
+        options.transparencyAmount = 100
+        options.webSnap = 0
+        
+        doc.exportDocument(meuArquivo, ExportType.SAVEFORWEB, options)
+    
+    }
+
+
+var nome = doc.name.replace(/.\w+$/,"")
+
+salvarGIF(nome)
+alert("Gif salvo! Tenha um dia animado. :)") 
